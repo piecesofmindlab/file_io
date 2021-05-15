88887ee222d903d07528cd648f81e353fffcce83
Mark Lescroart <mark.lescroart@gmail.com>
2021-05-15 11:40:45 -0700

    FIX: remove automatic install of requirements; doesn't seem to play well with conda environment

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as fid:
-	requirements = fid.readlines()
-	requirements = [x.strip() for x in requirements]
+# with open('requirements.txt') as fid:
+# 	requirements = fid.readlines()
+# 	requirements = [x.strip() for x in requirements]
 
 setup(
     name='file_io',
-    version='0.0.1',
+    version='0.1.0',
     packages=find_packages(),
     long_description=open('README.md').read(),
-    install_requires=requirements,
+    #install_requires=requirements,
     #include_package_data=True,
 )
